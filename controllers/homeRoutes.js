b539c2b543cb2b3c621d00fa37e47fe0076fec51
Micah Conn <mcoxconn@gmail.com>
2021-09-14 12:44:20 -0500

    function function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -107,6 +107,7 @@ router.get('/gallery', (req, res) => {
 
   res.render('gallery');
 });
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -117,4 +118,13 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
+});
 module.exports = router;
