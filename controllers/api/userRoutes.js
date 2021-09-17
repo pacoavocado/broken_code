f5e7f7ebc7a15c83b04df146b4c817fcc635c313
Micah Conn <mcoxconn@gmail.com>
2021-09-17 08:58:13 -0500

    morning push

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
   try {
@@ -16,55 +17,36 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!tourData) {
-//       res.status(404).json({ message: 'No show user with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(userData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 
 
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
-
+    
     if (!userData) {
       res
+      .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+      
+      const validPassword = await userData.checkPassword(req.body.password);
+      
+      if (!validPassword) {
+        res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
+    
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
+    
   } catch (err) {
     res.status(400).json(err);
   }
@@ -80,4 +62,23 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!tourData) {
+      res.status(404).json({ message: 'No show user with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
